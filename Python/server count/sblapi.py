215a3497a97a325dc5bcbdd8cccff7085c8a8b9b
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-10-09 19:52:38 -0500

    Add files via upload

---
+++ Python/server count/sblapi.py
@@ -0,0 +1,13 @@
+import requests
+
+def sblapi(botid:str,token:str,servercount:str):
+	url = "http://shieldbotlist.tk/api/auth/stats/" + botid
+	payload = "{\"server_count\": }" + servercount
+	headers = {
+  	'authorization': token,
+ 	 'Content-Type': 'application/json'
+	}
+	response = requests.request("POST", url, headers=headers, data = payload)
+	print("Updated server count")
+
+sblservercountapi("749825332470874203", "n1S6e7BRce1OdVE3p8fu", "1")
