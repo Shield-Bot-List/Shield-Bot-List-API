5f71d2fdfcbd8e2e9cf568f3576730e3d78e2d35
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2022-02-17 16:30:46 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SBLApi',
+    version='0.1.1',
+    description='API for shieldbotlist.website.',
+    url='https://github.com/Shield-Bot-List/Shield-Bot-List-API/tree/py-api',
+    author='fire',
+    author_email='help@shieldbotlist.website',
+    license='ISC',
+    packages=['SBLApi'],
+)
